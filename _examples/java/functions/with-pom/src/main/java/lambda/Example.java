0d3dd1afa1899913b4806434d8974c723002e52d
Szymon Karnecki <szymon.karnecki@gmail.com>
2016-03-15 11:08:20 +0100

    add java env variables. Closes #272

--- _examples/java/functions/with-pom/src/main/java/lambda/Example.java
+++ _examples/java/functions/with-pom/src/main/java/lambda/Example.java
@@ -2,6 +2,11 @@ package lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Example {
 
     public static class ExampleRequest {
@@ -25,6 +30,7 @@ public class Example {
 
     public static class ExampleResponse {
         String hello;
+        String token;
 
         public String getHello() {
             return hello;
@@ -34,8 +40,17 @@ public class Example {
             this.hello = hello;
         }
 
-        public ExampleResponse(String hello) {
+        public String getToken() {
+            return token;
+        }
+
+        public void setToken(String token) {
+            this.token = token;
+        }
+
+        public ExampleResponse(String hello, String token) {
             this.hello = hello;
+            this.token = token;
         }
 
         public ExampleResponse() {
@@ -43,6 +58,38 @@ public class Example {
     }
 
     public ExampleResponse handler(ExampleRequest event, Context context) {
-        return new ExampleResponse(event.getHello());
+        String token = "";
+        try {
+            token = (String) getProperty("LOGGLY_TOKEN");
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+        return new ExampleResponse(event.getHello(), token);
+    }
+
+    private Object getProperty(String key) throws IOException {
+        Object result = null;
+        InputStream inputStream = null;
+        try {
+            Properties prop = new Properties();
+            String propFileName = "apex.properties";
+
+            inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+            if (inputStream != null) {
+                prop.load(inputStream);
+            } else {
+                throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+            }
+
+            result = prop.get(key);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return result;
     }
 }
